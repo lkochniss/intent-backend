4eedd1dfddb2c496dba6fb508794fbf077a51f49
lkochniss <lukas.kochniss@gmail.com>
2015-08-13 19:31:29 +0200

    #4 added missing isActive and RoleInterface, added image routing

--- project/src/AppBundle/Entity/Role.php
+++ project/src/AppBundle/Entity/Role.php
@@ -4,11 +4,12 @@ namespace AppBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\Role\RoleInterface;
 
 /**
  * Role
  */
-class Role
+class Role implements RoleInterface
 {
     /**
      * @var integer
