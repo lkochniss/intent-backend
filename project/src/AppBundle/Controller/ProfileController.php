cd893a2ebc90ebee49fbf7b8f522b1aa8694f664
lkochniss <lukas.kochniss@gmail.com>
2015-08-23 23:52:55 +0200

    #22 add profile defaults

---
+++ project/src/AppBundle/Controller/ProfileController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Profile;
+use AppBundle\Form\Type\ProfileType;
+
+class ProfileController extends AbstractCrudController
+{
+    /**
+     * @return Profile
+     */
+    protected function createNewEntity()
+    {
+        return new Profile();
+    }
+
+    /**
+     * @return ProfileType
+     */
+    protected function getFormType()
+    {
+        return new ProfileType();
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTemplateBasePath()
+    {
+        return 'Profile';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getEntityName()
+    {
+        return 'AppBundle\Entity\Profile';
+    }
+
+    /**
+     * @return string
+     */
+    protected function getRoutePrefix()
+    {
+        return 'intent_backend_profile';
+    }
+
+    protected function getTranslationDomain()
+    {
+        return 'profile';
+    }
+
+    public function showAction($id)
+    {
+        return $this->render(
+            ':Profile:show.html.twig',
+            array(// ...
+            )
+        );
+    }
+}
