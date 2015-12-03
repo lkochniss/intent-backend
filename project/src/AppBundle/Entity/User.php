c20d10ff81f3bc56c219aedbbdb103d9718b63e7
lkochniss <lukas.kochniss@gmail.com>
2015-12-03 18:19:12 +0100

    #90 remove ORM

--- project/src/AppBundle/Entity/User.php
+++ project/src/AppBundle/Entity/User.php
@@ -6,7 +6,6 @@
 namespace AppBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 use Symfony\Component\Security\Core\User\EquatableInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
