8c50a6cd0cd273f206c3408ecf0deb11ce601f3c
lkochniss <lukas.kochniss@gmail.com>
2016-02-05 14:50:45 +0100

    reduce some code because its doubled due to abstract article

--- src/AppBundle/Entity/Article.php
+++ src/AppBundle/Entity/Article.php
@@ -14,41 +14,6 @@ use Symfony\Component\Validator\Constraints as Assert;
 class Article extends AbstractModel
 {
     /**
-     * @var String
-     *
-     * @Assert\NotBlank()
-     */
-    private $title;
-
-    /**
-     * @var String
-     */
-    private $slug;
-
-    /**
-     * @var String
-     *
-     * @Assert\NotBlank()
-     */
-    private $content;
-
-    /**
-     * @var \DateTime
-     *
-     * @Assert\DateTime()
-     */
-    private $publishAt;
-
-    /**
-     * @var Boolean
-     *
-     * @Assert\Type(
-     *     type="bool"
-     * )
-     */
-    private $published;
-
-    /**
      * @var Boolean
      *
      * @Assert\Type(
@@ -107,93 +72,6 @@ class Article extends AbstractModel
         $this->slideshow = false;
         $this->tags = new ArrayCollection();
         $this->versions = new ArrayCollection();
-    }
-
-    /**
-     * @param String $title The title of the entity.
-     * @return $this
-     */
-    public function setTitle($title)
-    {
-        $this->title = $title;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getTitle()
-    {
-        return $this->title;
-    }
-
-    /**
-     * @param String $slug The automatic generated slug.
-     * @return $this
-     */
-    public function setSlug($slug)
-    {
-        $this->slug = $slug;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getSlug()
-    {
-        return $this->slug;
-    }
-
-    /**
-     * @param String $content Set the written content.
-     * @return $this
-     */
-    public function setContent($content)
-    {
-        $this->content = $content;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getContent()
-    {
-        return $this->content;
-    }
-
-    /**
-     * @param \DateTime $publishAt Set the date when the entity should be published.
-     * @return $this
-     */
-    public function setPublishAt(\DateTime $publishAt)
-    {
-        $this->publishAt = $publishAt;
-
-        return $this;
-    }
-
-    /**
-     * @return \DateTime
-     */
-    public function getPublishAt()
-    {
-        return $this->publishAt;
-    }
-
-    /**
-     * @param boolean $published Set true when publishing to frontend returns true.
-     * @return $this
-     */
-    public function setPublished($published)
-    {
-        $this->published = $published;
-
-        return $this;
     }
 
     /**
