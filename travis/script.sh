034a8e938cca58b285574f786efd9b16590ddd3a
lkochniss <lukas.kochniss@gmail.com>
2016-05-13 10:28:40 +0200

    redo phpcs path

--- travis/script.sh
+++ travis/script.sh
@@ -3,11 +3,11 @@
 php bin/console security:check
 
 if [ "${CODESNIFF}" = "true" ] ; then
-    phpcs --standard=PSR1,PSR2 src -s;
+    php bin/phpcs --standard=PSR1,PSR2 src -s;
 fi
 
 #if [ "${CODECOV}" = "true" ] ; then
-#    php ./vendor/bin/phpunit --coverage-clover=coverage.xml;
+#    php bin/phpunit --coverage-clover=coverage.xml;
 #else
-#    php ./vendor/bin/phpunit;
+#    php bin/phpunit;
 #fi
