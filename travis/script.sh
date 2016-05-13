6a29d6c411157e87564f08ddcfb9b3f7ec601bae
lkochniss <lukas.kochniss@gmail.com>
2016-05-13 10:51:45 +0200

    remove phpcf from script.sh and move it to travis.

--- travis/script.sh
+++ travis/script.sh
@@ -2,10 +2,6 @@
 
 php bin/console security:check
 
-if [ "${CODESNIFF}" = "true" ] ; then
-    phpcs --standard=PSR1,PSR2 src -s;
-fi
-
 #if [ "${CODECOV}" = "true" ] ; then
 #   bin/phpunit --coverage-clover=coverage.xml;
 #else
