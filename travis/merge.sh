1e66fad5309c31c04e68b6f419289dbb15ef995c
lkochniss <lukas.kochniss@gmail.com>
2016-05-13 12:24:03 +0200

    debug merge.sh

--- travis/merge.sh
+++ travis/merge.sh
@@ -18,28 +18,16 @@ TO_BRANCH="develop"
 
 # Get the current branch
 export PAGER=cat
-$FROM_BRANCH=$(git log -n 1 --pretty=%d HEAD | cut -d"," -f3 | cut -d" " -f2 | cut -d")" -f1)
-echo "current branch is '$FROM_BRANCH'"
 
 # Create the URL to push merge to
 URL=$(git remote -v | head -n1 | cut -f2 | cut -d" " -f1)
 echo "Repo url is $URL"
 PUSH_URL="https://$GIT_USER:$GIT_PASS@${URL:6}"
 
-# Checkout the dev branch
-#git checkout $FROM_BRANCH && \
-#echo "Checking out $TO_BRANCH..." && \
-
-# Checkout the latest stable
-git fetch origin $TO_BRANCH:$TO_BRANCH && \
-git checkout $TO_BRANCH && \
-
-# Merge the dev into latest stable
-echo "Merging changes..." && \
-git merge $FROM_BRANCH && \
+git status
 
 # Push changes back to remote vcs
-echo "Pushing changes..." && \
-git push $PUSH_URL && \
-echo "Merge complete!" || \
-echo "Error Occurred. Merge failed"
+#echo "Pushing changes..." && \
+#git push $PUSH_URL && \
+#echo "Merge complete!" || \
+#echo "Error Occurred. Merge failed"
