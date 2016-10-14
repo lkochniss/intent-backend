2ff417c2c59986f2412adc4e005661d9d9ea5169
lkochniss <lukas.kochniss@gmail.com>
2016-10-14 12:58:10 +0200

    rename output in import and export command from entity to entities. also "fix" output in import from export category to import category.

--- src/AppBundle/Command/ImportDatabaseCommand.php
+++ src/AppBundle/Command/ImportDatabaseCommand.php
@@ -23,21 +23,21 @@ class ImportDatabaseCommand extends ContainerAwareCommand
         $this->setName('intent:database:import');
         $this->setDescription('Import database from XML source');
         $this->addOption('all', '-l', InputOption::VALUE_NONE, 'Import all entities.');
-        $this->addOption('art', '', InputOption::VALUE_NONE, 'Import entity article. Mind the dependencies.');
-        $this->addOption('cat', '', InputOption::VALUE_NONE, 'Import entity category.');
-        $this->addOption('dir', '', InputOption::VALUE_NONE, 'Import entity directory.');
-        $this->addOption('evt', '', InputOption::VALUE_NONE, 'Import entity event.');
-        $this->addOption('exp', '', InputOption::VALUE_NONE, 'Import entity expansion.');
-        $this->addOption('fra', '', InputOption::VALUE_NONE, 'Import entity franchise. Mind the dependencies.');
-        $this->addOption('gam', '', InputOption::VALUE_NONE, 'Import entity game. Mind the dependencies.');
-        $this->addOption('img', '', InputOption::VALUE_NONE, 'Import entity image. Mind the dependencies.');
-        $this->addOption('pag', '', InputOption::VALUE_NONE, 'Import entity page.');
-        $this->addOption('pro', '', InputOption::VALUE_NONE, 'Import entity profile. Mind the dependencies');
-        $this->addOption('pub', '', InputOption::VALUE_NONE, 'Import entity publisher.');
-        $this->addOption('rol', '', InputOption::VALUE_NONE, 'Import entity role.');
-        $this->addOption('stu', '', InputOption::VALUE_NONE, 'Import entity studio.');
-        $this->addOption('tag', '', InputOption::VALUE_NONE, 'Import entity tag.');
-        $this->addOption('usr', '', InputOption::VALUE_NONE, 'Import entity user. Mind the dependecies.');
+        $this->addOption('art', '', InputOption::VALUE_NONE, 'Import article entities. Mind the dependencies.');
+        $this->addOption('cat', '', InputOption::VALUE_NONE, 'Import category entities.');
+        $this->addOption('dir', '', InputOption::VALUE_NONE, 'Import directory entities.');
+        $this->addOption('evt', '', InputOption::VALUE_NONE, 'Import event entities.');
+        $this->addOption('exp', '', InputOption::VALUE_NONE, 'Import expansion entities.');
+        $this->addOption('fra', '', InputOption::VALUE_NONE, 'Import franchise entities. Mind the dependencies.');
+        $this->addOption('gam', '', InputOption::VALUE_NONE, 'Import game entities. Mind the dependencies.');
+        $this->addOption('img', '', InputOption::VALUE_NONE, 'Import image entities. Mind the dependencies.');
+        $this->addOption('pag', '', InputOption::VALUE_NONE, 'Import page entities.');
+        $this->addOption('pro', '', InputOption::VALUE_NONE, 'Import profile entities. Mind the dependencies');
+        $this->addOption('pub', '', InputOption::VALUE_NONE, 'Import publisher.');
+        $this->addOption('rol', '', InputOption::VALUE_NONE, 'Import role entities.');
+        $this->addOption('stu', '', InputOption::VALUE_NONE, 'Import studio entities.');
+        $this->addOption('tag', '', InputOption::VALUE_NONE, 'Import tag entities.');
+        $this->addOption('usr', '', InputOption::VALUE_NONE, 'Import user entities. Mind the dependecies.');
 
         return null;
     }
@@ -51,77 +51,77 @@ class ImportDatabaseCommand extends ContainerAwareCommand
     {
         if ($input->getOption('all') || $input->getOption('art')) {
             $this->getContainer()->get('app.article.service')->importEntities();
-            $output->writeln('exported article entity');
+            $output->writeln('imported article entities');
         }
 
         if ($input->getOption('all') || $input->getOption('cat')) {
             $this->getContainer()->get('app.category.service')->importEntities();
-            $output->writeln('exported category entity');
+            $output->writeln('imported category entities');
         }
 
         if ($input->getOption('all') || $input->getOption('dir')) {
             $this->getContainer()->get('app.directory.service')->importEntities();
-            $output->writeln('exported directory entity');
+            $output->writeln('imported directory entities');
         }
 
         if ($input->getOption('all') || $input->getOption('evt')) {
             $this->getContainer()->get('app.event.service')->importEntities();
-            $output->writeln('exported event entity');
+            $output->writeln('imported event entities');
         }
 
         if ($input->getOption('all') || $input->getOption('exp')) {
             $this->getContainer()->get('app.expansion.service')->importEntities();
-            $output->writeln('exported expansion entity');
+            $output->writeln('imported expansion entities');
         }
 
         if ($input->getOption('all') || $input->getOption('fra')) {
             $this->getContainer()->get('app.franchise.service')->importEntities();
-            $output->writeln('exported franchise entity');
+            $output->writeln('imported franchise entities');
         }
 
         if ($input->getOption('all') || $input->getOption('gam')) {
             $this->getContainer()->get('app.game.service')->importEntities();
-            $output->writeln('exported game entity');
+            $output->writeln('imported game entities');
         }
 
         if ($input->getOption('all') || $input->getOption('img')) {
             $this->getContainer()->get('app.image.service')->importEntities();
-            $output->writeln('exported image entity');
+            $output->writeln('imported image entities');
         }
 
         if ($input->getOption('all') || $input->getOption('pag')) {
             $this->getContainer()->get('app.page.service')->importEntities();
-            $output->writeln('exported page entity');
+            $output->writeln('imported page entities');
         }
 
         if ($input->getOption('all') || $input->getOption('pro')) {
             $this->getContainer()->get('app.profile.service')->importEntities();
-            $output->writeln('exported profile entity');
+            $output->writeln('imported profile entities');
         }
 
         if ($input->getOption('all') || $input->getOption('pub')) {
             $this->getContainer()->get('app.publisher.service')->importEntities();
-            $output->writeln('exported publisher entity');
+            $output->writeln('imported publisher entities');
         }
 
         if ($input->getOption('all') || $input->getOption('rol')) {
             $this->getContainer()->get('app.role.service')->importEntities();
-            $output->writeln('exported role entity');
+            $output->writeln('imported role entities');
         }
 
         if ($input->getOption('all') || $input->getOption('stu')) {
             $this->getContainer()->get('app.studio.service')->importEntities();
-            $output->writeln('exported studio entity');
+            $output->writeln('imported studio entities');
         }
 
         if ($input->getOption('all') || $input->getOption('tag')) {
             $this->getContainer()->get('app.tag.service')->importEntities();
-            $output->writeln('exported tag entity');
+            $output->writeln('imported tag entities');
         }
 
         if ($input->getOption('all') || $input->getOption('usr')) {
             $this->getContainer()->get('app.user.service')->importEntities();
-            $output->writeln('exported user entity');
+            $output->writeln('imported user entities');
         }
 
         return true;
