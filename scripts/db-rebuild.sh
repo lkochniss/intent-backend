40ea898fe70ba5593e4e34ebaa9381946a5eed23
lkochniss <lukas.kochniss@gmail.com>
2016-10-14 13:32:26 +0200

    bugfix tests by creating the database and loading fixtures. also add --if-exists flag to database:drop

--- scripts/db-rebuild.sh
+++ scripts/db-rebuild.sh
@@ -4,7 +4,7 @@ pushd "$(dirname $0)" > /dev/null
 
 cd ..
 
-bin/console do:da:dr --force
+bin/console do:da:dr --force --if-exists
 bin/console do:da:cr
 bin/console do:mi:mi -n
 bin/console do:fi:lo --fixtures src/AppBundle/DataFixtures/ORM/dev/ -n
