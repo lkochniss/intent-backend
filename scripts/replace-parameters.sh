9296f06f550dd9b4ee11bfb0c93e5b5f9aa645a4
lkochniss <lukas.kochniss@gmail.com>
2016-11-04 16:16:24 +0100

    Try to get GIT_BRANCH working

--- scripts/replace-parameters.sh
+++ scripts/replace-parameters.sh
@@ -7,7 +7,7 @@ cd ..
 sed -e "s/\$DB_HOST/$DB_HOST/" \
     -e "s/\$DB_PORT/$DB_PORT/" \
     -e "s/\$DB_NAME/$DB_NAME/" \
-    -e "s/\BRANCH_NAME/$BRANCH_NAME/" \
+    -e "s/\$BRANCH_NAME/$BRANCH_NAME/" \
     -e "s/\$DB_USER/$DB_USER/" \
     -e "s/\$DB_PASS/$DB_PASS/" \
     -e "s/\$LOCALE/$LOCALE/" \
