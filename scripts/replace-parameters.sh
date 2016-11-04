4bbb08c8bcb1e29cff7c48fadbd0f9d4e3923c44
lkochniss <lukas.kochniss@gmail.com>
2016-11-04 12:21:15 +0100

    Bugfix: Remove MAIL parameters. Add replace script for general usage.

---
+++ scripts/replace-parameters.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+pushd "$(dirname $0)" > /dev/null
+
+cd ..
+
+sed -e "s/\$DB_HOST/$DB_HOST/" \
+    -e "s/\$DB_PORT/$DB_PORT/" \
+    -e "s/\$DB_NAME/$DB_NAME/" \
+    -e "s/\$DB_USER/$DB_USER/" \
+    -e "s/\$DB_PASS/$DB_PASS/" \
+    -e "s/\$LOCALE/$LOCALE/" \
+    -e "s/\$SECRET/$SECRET/" \
+    -e "s/\$SESSION/$SESSION/" \
+    app/config/parameters.yml.dist > app/config/parameters.yml
--- travis/before-script.sh
+++ travis/before-script.sh
@@ -4,20 +4,7 @@ pushd "$(dirname $0)" > /dev/null
 
 cd ..
 
-sed -e "s/\$DB_HOST/$DB_HOST/" \
-    -e "s/\$DB_PORT/$DB_PORT/" \
-    -e "s/\$DB_NAME/$DB_NAME/" \
-    -e "s/\$DB_USER/$DB_USER/" \
-    -e "s/\$DB_PASS/$DB_PASS/" \
-    -e "s/\$MAIL_TRANS/$MAIL_TRANS/" \
-    -e "s/\$MAIL_HOST/$MAIL_HOST/" \
-    -e "s/\$MAIL_USER/$MAIL_USER/" \
-    -e "s/\$MAIL_PASS/$MAIL_PASS/" \
-    -e "s/\$LOCALE/$LOCALE/" \
-    -e "s/\$SECRET/$SECRET/" \
-    -e "s/\$SESSION/$SESSION/" \
-    app/config/parameters.yml.dist > app/config/parameters.yml
-
+./scripts/replace-parameters.sh
 
 php bin/console do:da:dr --force --if-exists
 php bin/console do:da:cr
