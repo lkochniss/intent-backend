34b66d481d8b8ce91dfe5f7f98f7e20c66ba92d3
lkochniss <lukas.kochniss@gmail.com>
2016-11-04 12:05:33 +0100

    Use replaceable parameters and update before-script

--- travis/before-script.sh
+++ travis/before-script.sh
@@ -4,7 +4,20 @@ pushd "$(dirname $0)" > /dev/null
 
 cd ..
 
-cp app/config/parameters.yml.dist app/config/parameters.yml
+sed -e "s/\$DB_HOST/$DB_HOST/" \
+    -e "s/\$DB_PORT/$DB_PORT/" \
+    -e "s/\$DB_NAME/$DB_NAME/" \
+    -e "s/\$DB_USER/$DB_USER/" \
+    -e "s/\$DB_USER/$DB_USER/" \
+    -e "s/\$MAIL_TRANS/$MAIL_TRANS/" \
+    -e "s/\$MAIL_HOST/$MAIL_HOST/" \
+    -e "s/\$MAIL_USER/$MAIL_USER/" \
+    -e "s/\$MAIL_PASS/$MAIL_PASS/" \
+    -e "s/\$LOCALE/$LOCALE/" \
+    -e "s/\$SECRET/$SECRET/" \
+    -e "s/\$SESSION/$SESSION/" \
+    app/config/parameters.yml.dist > app/config/parameters.yml
+
 
 php bin/console do:da:dr --force --if-exists
 php bin/console do:da:cr
