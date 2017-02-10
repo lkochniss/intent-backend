97869c64bcec8efec473fccf3c4e48e56d6fedd3
lkochniss <lukas.kochniss@gmail.com>
2017-02-10 09:23:25 +0100

    Sum up some doctrine versions

--- app/DoctrineMigrations/Version20170210081912.php
+++ app/DoctrineMigrations/Version20170210081912.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20150904100618 extends AbstractMigration
+class Version20170210081912 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -16,9 +16,10 @@ class Version20150904100618 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE image CHANGE description description LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE page DROP publish_at');
     }
 
     /**
@@ -27,8 +28,9 @@ class Version20150904100618 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE image CHANGE description description LONGTEXT NOT NULL');
+        $this->addSql('ALTER TABLE image CHANGE description description LONGTEXT NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE page ADD publish_at DATETIME DEFAULT NULL');
     }
 }
